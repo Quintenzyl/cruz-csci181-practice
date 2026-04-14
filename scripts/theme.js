d730d1abe1f2ec8d27db778f82530fa6250aec0c
Quintenzyl <albinosnowwolf@gmail.com>
2026-04-14 15:19:58 -0600

    !

--- scripts/theme.js
+++ scripts/theme.js
@@ -4,10 +4,14 @@ const theme_button = document.querySelector("#theme-toggle");
 // Selected message of button to change 
 const button_message = document.querySelector("#theme-message")
 
-// Function that changes text of button
+// Function that changes theme and text of button
 function changeTheme() {
   var element = document.body;
   element.classList.toggle("darkmode");
+  if (document.body.classList.contains("darkmode"))
+    button_message.textContent = "Change to Light Mode";
+  else
+    button_message.textContent = "Change to Dark Mode";
 } 
 
 
